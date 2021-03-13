a402cfcbd239d784c94981c48a1784829a38a371
aayushmishra0 <71607632+aayushmishra0@users.noreply.github.com>
2021-03-13 10:37:16 -0600

    Add files via upload

---
+++ Question.js
@@ -0,0 +1,13 @@
+display(){
+    this.title.html("MyQuiz Game");
+    this.title.position(350,0);
+
+    this.question.html("Question- what do you drink every day and you need the most?");
+    this.question.position(150,80);
+    this.option1.html("1: Milk")
+    this.option1.position(150,100)
+    this.option2.html("2: Water");
+    this.option2.position(150,120);
+
+    
+}
